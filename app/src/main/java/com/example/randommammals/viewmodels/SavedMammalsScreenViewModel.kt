4615c81cdea4cc5cd49e8f36ea5aae36363def54
Smig <ahmedharoun370@gmail.com>
2024-01-01 14:09:35 -0500

    Added ui state to SavedMammalsScreenViewModel

--- app/src/main/java/com/example/randommammals/viewmodels/SavedMammalsScreenViewModel.kt
+++ app/src/main/java/com/example/randommammals/viewmodels/SavedMammalsScreenViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.randommammals.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.randommammals.data.RandomMammalsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,6 +15,8 @@ class SavedMammalsScreenViewModel @Inject constructor(
     private val repository: RandomMammalsRepository
 ): ViewModel() {
 
+    var uiState: SavedMammalsScreenUiState by mutableStateOf(SavedMammalsScreenUiState(listOf()))
+    private set
     fun getSavedMammals(){
         //TODO
     }
