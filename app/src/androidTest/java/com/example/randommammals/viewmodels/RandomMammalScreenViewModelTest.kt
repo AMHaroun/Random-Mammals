6c63ebfa1902fd4ddb7d64fdb8901cb5d0d064f8
Smig <ahmedharoun370@gmail.com>
2023-12-31 19:19:08 -0500

    Fixed flaky coroutine tests in RandomMammalScreenViewModelTest.kt

--- app/src/androidTest/java/com/example/randommammals/viewmodels/RandomMammalScreenViewModelTest.kt
+++ app/src/androidTest/java/com/example/randommammals/viewmodels/RandomMammalScreenViewModelTest.kt
@@ -3,6 +3,12 @@ package com.example.randommammals.viewmodels
 import com.example.randommammals.di.FakeRandomMammalsRepository
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -25,27 +31,45 @@ class RandomMammalScreenViewModelTest{
         viewModel = RandomMammalScreenViewModel(repository)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getRandomMammal_uiStateIsSuccess(){
+    fun getRandomMammal_uiStateIsSuccess() = runTest{
+
+        val dispatcher = StandardTestDispatcher()
+
+        Dispatchers.setMain(dispatcher)
 
         viewModel.getRandomMammal()
+
+        dispatcher.scheduler.advanceUntilIdle()
 
         val uiState  = viewModel.uiState
-
         assertTrue(uiState is RandomMammalScreenUiState.Success)
+
+        Dispatchers.resetMain()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getRandomMammal_networkError_uiStateIsError(){
+    fun getRandomMammal_networkError_uiStateIsError() = runTest{
         repository.toggleNetworkErrors()
 
+        val dispatcher = StandardTestDispatcher()
+
+        Dispatchers.setMain(dispatcher)
+
         viewModel.getRandomMammal()
+
+        dispatcher.scheduler.advanceUntilIdle()
+
 
         repository.toggleNetworkErrors()
 
         val uiState = viewModel.uiState
 
         assertTrue(uiState is RandomMammalScreenUiState.Error)
+
+        Dispatchers.resetMain()
     }
 
 }
