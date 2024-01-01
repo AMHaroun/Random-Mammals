3ea61b15e13d8ec186728054de2465bd99f18bc4
Smig <ahmedharoun370@gmail.com>
2024-01-01 14:12:30 -0500

    Fixed Constants BaseURL error  with retrofit -> Had to add / at the end of each url

--- app/src/main/java/com/example/randommammals/util/Constants.kt
+++ app/src/main/java/com/example/randommammals/util/Constants.kt
@@ -2,8 +2,8 @@ package com.example.randommammals.util
 
 object Constants {
 
-    const val RANDOMDUCK_URL = "https://random-d.uk/api/v2"
-    const val RANDOMFOX_URL = "https://https://randomfox.ca"
-    const val THECATAPI_URL = "https://api.thecatapi.com/v1"
+    const val RANDOMDUCK_URL = "https://random-d.uk/api/v2/"
+    const val RANDOMFOX_URL = "https://https://randomfox.ca/"
+    const val THECATAPI_URL = "https://api.thecatapi.com/v1/"
 
 }
