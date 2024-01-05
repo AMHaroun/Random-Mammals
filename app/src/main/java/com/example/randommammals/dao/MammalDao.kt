e205847a2a354944cbb89c7325c1cb7d1eaa58cb
Smig <ahmedharoun370@gmail.com>
2024-01-05 11:00:29 -0500

    Added Mammal data access object + renamed SavedMammal Entity

--- app/src/main/java/com/example/randommammals/dao/MammalDao.kt
+++ app/src/main/java/com/example/randommammals/dao/MammalDao.kt
@@ -4,14 +4,19 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.randommammals.entity.Mammal
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MammalDao {
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMammal(mammal: Mammal)
 
     @Delete
     suspend fun deleteMammal(mammal: Mammal)
+
+    @Query("SELECT * FROM mammal")
+    fun getMammals(): Flow<List<Mammal>>
 }
